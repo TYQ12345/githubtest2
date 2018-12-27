e33ca279cd52e40830ad7bd7c0776e5ed4789bda
ITVictor <1402523791@qq.com>
2018-12-27 16:10:35 +0800

    修改数据操作的bug

--- app/src/main/java/cn/gdptc/xxgcx/hwh/githubtest/MainActivity.java
+++ app/src/main/java/cn/gdptc/xxgcx/hwh/githubtest/MainActivity.java
@@ -1,13 +1,31 @@
 package cn.gdptc.xxgcx.hwh.githubtest;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
 
+public class MainActivity extends Activity {
+    private List<Account> list;
+    private AccountDao dao;
+    private EditText nameET;
+    private EditText balanceET;
+//    private MyAdapter adapter;
+    private ListView accountLV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+/*        initView();
+        dao=new AccountDao(this);
+        list=dao.queryAll();
+        adapter=new MyAdapter();*/
+
+
+
     }
 }
