6f1a027bf0122808f84a442439ad9108bb281df3
ITVictor <1402523791@qq.com>
2018-12-27 10:43:04 +0800

    创建数据库

---
+++ app/src/main/java/cn/gdptc/xxgcx/hwh/githubtest/dao/MyHelper.java
@@ -0,0 +1,25 @@
+package cn.gdptc.xxgcx.hwh.githubtest.dao;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+//创建数据库
+public class MyHelper extends SQLiteOpenHelper {
+     public MyHelper(Context context){
+         super(context,"itcast.db",null,2);
+     }
+    
+     public void onCreate(SQLiteDatabase db){
+         System.out.println("onCreate");
+         db.execSQL("CREATE TABLE account (_id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                 "name VARCHAR(20)," +
+                 "balance INTEGER)");
+     }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        System.out.println("onUpgrade");
+    }
+
+
+}
