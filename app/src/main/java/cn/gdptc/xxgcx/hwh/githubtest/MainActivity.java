fb5219b13993cb4070f445e36e9e21535eff1978
ITVictor <1402523791@qq.com>
2018-12-27 23:34:08 +0800

    编写MainActivity_add

--- app/src/main/java/cn/gdptc/xxgcx/hwh/githubtest/MainActivity.java
+++ app/src/main/java/cn/gdptc/xxgcx/hwh/githubtest/MainActivity.java
@@ -19,47 +19,49 @@ public class MainActivity extends Activity {
     private AccountDao dao;
     private EditText nameET;
     private EditText balanceET;
-    private MyAdapter adapter;
+    private MyAdapter adapter = new MyAdapter();
     private ListView accountLV;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         initView();
-        dao=new AccountDao(this);
-        list=dao.queryAll();
-        adapter=new MyAdapter();
-        accountLV.setAdapter((ListAdapter)adapter);
+        dao = new AccountDao(this);
+        list = dao.queryAll();
+        accountLV.setAdapter((ListAdapter) adapter);
     }
 
     private void initView() {
-        accountLV=findViewById(R.id.accountLV);
-        nameET=findViewById(R.id.nameET);
-        balanceET=findViewById(R.id.balanceET);
+        accountLV = findViewById(R.id.accountLV);
+        nameET = findViewById(R.id.nameET);
+        balanceET = findViewById(R.id.balanceET);
         accountLV.setOnItemClickListener(new MyOnItemClickListener());
     }
 
 
     public void add(View v) {
-        String name=nameET.getText().toString().trim();
-        String balance=balanceET.getText().toString().trim();
-            Account a=new Account(name,balance.equals("")?0:
+        String name = nameET.getText().toString().trim();
+        String balance = balanceET.getText().toString().trim();
+        Account a = new Account(name, balance.equals("") ? 0 :
                 Integer.parseInt(balance));
         dao.insert(a);
         list.add(a);
         adapter.notifyDataSetChanged();
-        accountLV.setSelection(accountLV.getCount()-1);
+        accountLV.setSelection(accountLV.getCount() - 1);
         nameET.setText("");
         balanceET.setText("");
     }
 
 
+
+
     private class MyOnItemClickListener implements AdapterView.OnItemClickListener {
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            Account a= (Account) parent.getItemAtPosition(position);
-            Toast.makeText(getApplicationContext(),a.toString(),
+            Account a = (Account) parent.getItemAtPosition(position);
+            Toast.makeText(getApplicationContext(), a.toString(),
                     Toast.LENGTH_SHORT);
         }
     }
